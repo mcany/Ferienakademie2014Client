73d83fc9050d80d7a2deb01fa6ff07292e52402b
explicat <explicat@btl1.de>
2014-09-25 08:25:47 -0700

    Model intialized

---
+++ FitFritz/app/src/main/java/ferienakademie/de/fitfritz/model/Activity.java
@@ -0,0 +1,13 @@
+package ferienakademie.de.fitfritz.model;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by explicat on 9/25/14.
+ */
+public class Activity {
+
+    private Sports sports;
+    private Map<Class, List<SensorData>> sensorData;
+}
---
+++ FitFritz/app/src/main/java/ferienakademie/de/fitfritz/model/Sports.java
@@ -0,0 +1,11 @@
+package ferienakademie.de.fitfritz.model;
+
+/**
+ * Created by explicat on 9/25/14.
+ */
+public enum Sports {
+    RUNNING,
+    BIKING,
+    WALKING,
+    HIKING;
+}
