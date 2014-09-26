81ade6906793945eea1225adf26aa6dcc25e2450
Isabel Max <isabel.max@in.tum.de>
2014-09-26 14:36:51 +0200

    Added DatabaseHandler

---
+++ FitFritz/app/src/main/java/ferienakademie/de/fitfritz/controller/DatabaseHandler.java
@@ -0,0 +1,111 @@
+package ferienakademie.de.fitfritz.controller;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
+import android.location.Location;
+import android.util.Log;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import ferienakademie.de.fitfritz.model.LocationData;
+
+/**
+ * Created by isabelmax on 26.09.14.
+ */
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    private static final String LOGCAT = "logcat";
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "SensorData";
+    private static final String[] TABLE_NAMES = {"GPSData", "HeartRateData"};
+
+    //columns gps data table
+    private static final String COL_ID = "id";
+    private static final String COL_TIMESTAMP = "timeStamp";
+    private static final String COL_LONGITUDE = "longitude";
+    private static final String COL_LATITUDE = "latitude";
+    private static final String COL_ALTITUDE = "altitude";
+    private static final String COL_SPEED = "speed";
+    private static final String COL_SYNC_SERVER = "synchronized";
+
+    private static final String[] COLUMS_GPSDATA = {COL_ID, COL_TIMESTAMP, COL_LONGITUDE, COL_LATITUDE, COL_ALTITUDE, COL_SPEED, COL_SYNC_SERVER};
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String CREATE_GPS_TABLE = "CREATE TABLE " + TABLE_NAMES[0] + " ( " + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COL_TIMESTAMP + " INTEGER, " + COL_LONGITUDE + " REAL, " + COL_LATITUDE
+                + " REAL, " + COL_ALTITUDE + " REAL, " + COL_SPEED + " REAL, " + COL_SYNC_SERVER + " INTEGER);";
+        sqLiteDatabase.execSQL(CREATE_GPS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+        // drop older database table if exists
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAMES[0]);
+
+        // create new database table
+        this.onCreate(sqLiteDatabase);
+    }
+
+    // insert a list of GPS Data to the database
+    public void insertGPSDate(LocationData gpsData) {
+
+        String insertStatement = "INSERT INTO " + TABLE_NAMES[0] + "(" ;
+        for (int i = 1; i < COLUMS_GPSDATA.length; i++) {
+           if(i > 1) {
+               insertStatement += ", ";
+           }
+            insertStatement += COLUMS_GPSDATA[i];
+        }
+        insertStatement += ") VALUES (";
+
+        // current date in seconds (not ms!!) since 01.01.1970
+        insertStatement += new Date().getTime()/1000 + ", ";
+        insertStatement += gpsData.getLatLng().longitude + ", ";
+        insertStatement += gpsData.getLatLng().latitude + ", ";
+        insertStatement += gpsData.getmLocation().getAltitude() + ", ";
+        insertStatement += gpsData.getmLocation().getSpeed() + ", ";
+        insertStatement += "0);";
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(insertStatement);
+    }
+
+    // used for testing -> to be deleted afterwards
+    public Map<Integer, Location> getUnsyncedLocationData() {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.query(TABLE_NAMES[0], COLUMS_GPSDATA, COL_SYNC_SERVER + " = 0", new String[]{}, null, null, null);
+        cursor.moveToFirst();
+        try {
+            while(!cursor.isAfterLast()) {
+                int timeStamp = cursor.getInt(1);
+                double longitudeString = cursor.getDouble(2);
+                double latitudeString = cursor.getDouble(3);
+                double altitudeString = cursor.getDouble(4);
+                double speedString = cursor.getDouble(5);
+                int isSyncedInt = cursor.getInt(6);
+
+                Log.d(LOGCAT, "Time: " + timeStamp + " Long: " + longitudeString + " Lat: " + latitudeString + " Alt: " + altitudeString + " Speed: " + speedString + " syncstatus: " + (isSyncedInt == 1));
+                cursor.moveToNext();
+            }
+        }
+        finally {
+            cursor.close();
+        }
+        return null;
+    }
+
+}
