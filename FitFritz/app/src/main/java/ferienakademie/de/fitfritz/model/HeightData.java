2da9a247e5d9d0b5fcec14c7f7b9d15eb17797e8
Benedikt Lorch <benedikt.lorch@fau.de>
2014-09-27 00:30:56 -0700

    Renamed project to neverrest

--- FitFritz/app/src/main/java/ferienakademie/de/fitfritz/model/HeightData.java
+++
@@ -1,7 +0,0 @@
-package ferienakademie.de.fitfritz.model;
-
-/**
- * Created by explicat on 9/25/14.
- */
-public class HeightData {
-}
---
+++ nEVERrEST/app/src/main/java/de/ferienakademie/neverrest/model/SensorData.java
@@ -0,0 +1,19 @@
+package de.ferienakademie.neverrest.model;
+
+import java.util.Date;
+
+/**
+ * Created by explicat on 9/25/14.
+ */
+public abstract class SensorData {
+
+    private Date creationDate;
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
