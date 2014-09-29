3097cb32e6b96b64f4ee5364aafff24e09cd0258
bradrulck <christoph.luckner@gmail.com>
2014-09-29 13:57:01 +0200

    ActiveChallengesAdded

--- nEVERrEST/app/src/main/java/de/ferienakademie/neverrest/view/ActiveChallengesActivity.java
+++ nEVERrEST/app/src/main/java/de/ferienakademie/neverrest/view/ActiveChallengesActivity.java
@@ -38,27 +38,30 @@ public class ActiveChallengesActivity extends ListActivity {
     }
 
     protected void onStart(){
+        super.onStart();
 
     }
 
     protected void onRestart(){
+        super.onRestart();
 
     }
 
     protected void onResume(){
+        super.onResume();
 
     }
 
     protected void onPause(){
-
+        super.onPause();
     }
 
     protected void onStop(){
-
+        super.onStop();
     }
 
     protected void onDestroy(){
-
+        super.onDestroy();
     }
 }
 
