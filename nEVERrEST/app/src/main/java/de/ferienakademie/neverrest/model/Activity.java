e7334fa18511c83ddbfae16e94b2045c2e7398e0
explicat <explicat@btl1.de>
2014-09-29 13:08:30 -0700

    Moved model classes from shared library to model package

---
+++ nEVERrEST/app/src/main/java/de/ferienakademie/neverrest/model/Activity.java
@@ -0,0 +1,123 @@
+package de.ferienakademie.neverrest.model;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = Activity.TABLE_ACTIVITY)
+public final class Activity {
+
+    public final static String TABLE_ACTIVITY = "activity";
+    public final static String COL_UUID = "uuid";
+    public final static String COL_DURATION = "duration";
+    public final static String COL_ENERGY = "energy";
+    public final static String COL_ADDITIONAL_LIFETIME = "additional_lifetime";
+    public final static String COL_TIMESTAMP = "timestamp";
+    public final static String COL_USER_UUID = "user_uuid";
+    public final static String COL_TYPE = "type";
+
+
+    @DatabaseField(columnName = COL_UUID, id = true)
+    private String uuid;
+
+    @DatabaseField(columnName = COL_DURATION)
+    private Double duration;
+
+    @DatabaseField(columnName = COL_ENERGY)
+    private Double energy;
+
+    @DatabaseField(columnName = COL_ADDITIONAL_LIFETIME)
+    private Double additionalLiftime;
+
+    @DatabaseField(columnName = COL_TIMESTAMP)
+    private Long timestamp;
+
+    @DatabaseField(columnName = COL_USER_UUID)
+    private String userUuid;
+
+    @DatabaseField(columnName = COL_TYPE)
+    private SportsType type;
+
+    public Activity() {
+        // ORMLite needs a no-arg constructor
+    }
+
+
+    public Activity(
+            String uuid,
+            Long timestamp,
+            Double duration,
+            Double energy,
+            Double additionalLiftime,
+            String userUuid,
+            SportsType type) {
+
+        this.uuid = uuid;
+        this.timestamp = timestamp;
+        this.duration = duration;
+        this.energy=energy;
+        this.additionalLiftime=additionalLiftime;
+        this.userUuid = userUuid;
+        this.type = type;
+
+    }
+
+
+    public String getUuid() {
+        return uuid;
+    }
+
+
+    public Double getDuration() {
+        return duration;
+    }
+
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+
+    public Double getEnergy() {
+        return energy;
+    }
+
+
+    public String getUserUuid() {
+        return userUuid;
+    }
+
+
+    public SportsType getType() {
+        return type;
+    }
+
+    public Double getAdditionalLiftime() {
+        return additionalLiftime;
+    }
+
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public void setDuration(Double duration) {
+        this.duration = duration;
+    }
+
+    public void setEnergy(Double energy) {
+        this.energy = energy;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setUserUuid(String userUuid) {
+        this.userUuid = userUuid;
+    }
+
+    public void setType(SportsType type) {
+        this.type = type;
+    }
+
+}
---
+++ nEVERrEST/app/src/main/java/de/ferienakademie/neverrest/model/SportsType.java
@@ -0,0 +1,9 @@
+package de.ferienakademie.neverrest.model;
+
+/**
+ * Created by explicat on 9/29/14.
+ */
+public enum SportsType {
+    RUNNING,
+    CYCLING;
+}
