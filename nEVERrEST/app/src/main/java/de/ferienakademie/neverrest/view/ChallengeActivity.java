1456fa6380b5aba34fbdc7680a3501abc7f83383
Larissa <la8895@web.de>
2014-09-29 14:59:39 +0200

    ChallengeActivity added

---
+++ nEVERrEST/app/src/main/java/de/ferienakademie/neverrest/view/ChallengeActivity.java
@@ -0,0 +1,52 @@
+package de.ferienakademie.neverrest.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import de.ferienakademie.neverrest.R;
+import de.ferienakademie.neverrest.shared.beans.Challenge;
+
+public class ChallengeActivity extends Activity {
+
+    Challenge challenge;
+    TextView heading;
+    ImageView challengeImage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge);
+       Challenge dummyChallenge = new Challenge("idsaf", "Roberti Golumm", de.ferienakademie.neverrest.shared.beans.Activity.Type.CYCLING,"des",100.0,100);
+        challenge = dummyChallenge;
+        heading = (TextView) findViewById(R.id.heading);
+        heading.setText(challenge.getTitle());
+
+
+     //   challenge.getPercentageCompleted();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+      //  getMenuInflater().inflate(R.menu.challenge, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
