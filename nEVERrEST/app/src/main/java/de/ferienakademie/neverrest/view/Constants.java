2e0c5f4a84c759b95b0aef6d16b263af70efff4f
richrobe <robert.richer94@googlemail.com>
2014-10-01 11:09:08 +0200

    Master version

---
+++ nEVERrEST/app/src/main/java/de/ferienakademie/neverrest/view/Constants.java
@@ -0,0 +1,14 @@
+package de.ferienakademie.neverrest.view;
+
+/**
+ * Created by Robert on 01.10.2014.
+ */
+public class Constants {
+
+    public static final int ACTIVITY_MAIN_MENU = 0;
+    public static final int ACTIVITY_PROFILE = 1;
+    public static final int ACTIVITY_CHALLENGE_OVERVIEW = 2;
+    public static final int ACTIVITY_SETTINGS = 3;
+    public static final int ACTIVITY_CURRENT = 4;
+
+}
