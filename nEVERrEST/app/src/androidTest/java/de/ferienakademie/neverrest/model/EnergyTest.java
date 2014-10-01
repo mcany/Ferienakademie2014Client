8e4352a39492e91bd1c23a96941341c92345bf71
Frederik <f@polarapps.de>
2014-10-01 18:02:53 +0200

    another private method test from testing class (jetz wo I woas wis goat)

--- nEVERrEST/app/src/androidTest/java/de/ferienakademie/neverrest/model/EnergyTest.java
+++ nEVERrEST/app/src/androidTest/java/de/ferienakademie/neverrest/model/EnergyTest.java
@@ -30,13 +30,13 @@ public class EnergyTest extends TestCase{
         double result = 0;
         //test private methods
         try {
-            Method m = Class.forName("de.ferienakademie.neverrest.model.Energy").getDeclaredMethod("grundumsatz", User.class);
+            Method m = Class.forName("de.ferienakademie.neverrest.model.Energy").getDeclaredMethod("kiloJouleToCalories", User.class);
             m.setAccessible(true);
-            grundumsatz = ((Double)m.invoke(null, user)).doubleValue();
+            result = ((Double)m.invoke(null, 1.0)).doubleValue();
         } catch (Exception e) {
             Log.e("Error",e+"");
         }
-        assertEquals(9703.68, grundumsatz, 0.001);
+        assertEquals(239.005736, result, 0.001);
     }
 
 }
