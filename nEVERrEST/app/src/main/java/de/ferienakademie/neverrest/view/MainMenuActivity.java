6a126457989d7a1f3e3bddda004c40f58a5fc4d4
bradrulck <christoph.luckner@gmail.com>
2014-10-01 17:07:26 +0200

    ActiveChallengesAdded

--- nEVERrEST/app/src/main/java/de/ferienakademie/neverrest/view/MainMenuActivity.java
+++ nEVERrEST/app/src/main/java/de/ferienakademie/neverrest/view/MainMenuActivity.java
@@ -8,6 +8,7 @@ import android.content.ServiceConnection;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -74,6 +75,7 @@ public class MainMenuActivity extends FragmentActivity
     private String mProvider;
     private Marker mMarker;
     private float[] mDistance = new float[1];
+    private MediaPlayer mMediaPlayer;
 
 
     ///////// NAVIGATION DRAWER STUFF /////////
@@ -114,6 +116,8 @@ public class MainMenuActivity extends FragmentActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        mMediaPlayer = MediaPlayer.create(this, R.raw.sky);
+        mMediaPlayer.start();
         mSportsType = (SportsType) getIntent().getSerializableExtra(MainMenuActivity.SPORTS_TYPE);
         mIsCreated = true;
 
